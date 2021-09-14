9bda7ad4a622ec1e4e0e7a1b4b313ec5dc264827
Daniel <5-Daniel@users.noreply.gitlab.bock.local>
2021-09-14 07:19:42 +0200

    Make admin settings.

--- build.ps1
+++ build.ps1
@@ -1,1 +1,4 @@
-dotnet-gitversion | ConvertFrom-Json | % { Update-ModuleManifest -Path ./Modules/SonarQubePS2/SonarQubePS2.psd1 -ModuleVersion $_.MajorMinorPatch }
+dotnet-gitversion | ConvertFrom-Json | % { 
+  echo "Set ModuleVersion to $_.MajorMinorPatch"
+  Update-ModuleManifest -Path ./Modules/SonarQubePS2/SonarQubePS2.psd1 -ModuleVersion $_.MajorMinorPatch 
+}
